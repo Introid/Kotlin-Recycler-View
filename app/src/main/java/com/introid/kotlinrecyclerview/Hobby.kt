f8637c9e668cb474f1eee4dd51dd9acf4a57c8aa
prakashRanjan19 <prakash.r19500@gmail.com>
2020-04-17 14:39:49 +0530

    master

---
+++ app/src/main/java/com/introid/kotlinrecyclerview/Hobby.kt
@@ -0,0 +1,26 @@
+package com.introid.kotlinrecyclerview
+
+data class Hobby(var title: String)
+object Supplier{
+    val hobbies= listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Fucking"),
+        Hobby("Masterbating"),
+        Hobby("porn"),
+        Hobby("walking"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Fucking"),
+        Hobby("Masterbating"),
+        Hobby("porn"),
+        Hobby("walking"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Fucking"),
+        Hobby("Masterbating"),
+        Hobby("porn"),
+        Hobby("walking")
+    )
+}
